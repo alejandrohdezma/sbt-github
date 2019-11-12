175d78c6bf2e7933fb1b7a15e113e1f68b6e5d7e
Alejandro Hernández <info@alejandrohdezma.com>
2019-11-12 15:44:14 +0100

    Add methods for getting developer and org info from current user

--- src/main/scala/com/alejandrohdezma/sbt/me/github/CurrentUser.scala
+++ src/main/scala/com/alejandrohdezma/sbt/me/github/CurrentUser.scala
@@ -1,4 +1,15 @@
 package com.alejandrohdezma.sbt.me.github
 
+import sbt.{url, Developer}
+
 /** Represents the current authenticated user from Github */
-final case class CurrentUser(login: String, name: String, email: String, html_url: String)
+final case class CurrentUser(login: String, name: String, email: String, html_url: String) {
+
+  /** The current user as an sbt `Developer` */
+  def developer: Developer = Developer(login, name, email, url(html_url))
+
+  /** The user's organization extracted from its email */
+  def organization: String =
+    email.substring(email.indexOf("@") + 1).split('.').reverse.mkString(".")
+
+}
