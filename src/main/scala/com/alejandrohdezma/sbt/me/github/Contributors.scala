e5a3fe00daa0679befbfa147a75daa1629ec2d0b
Alejandro Hernández <info@alejandrohdezma.com>
2020-01-13 16:35:06 +0100

    Add method to obtain the list of contributors in markdown format

--- src/main/scala/com/alejandrohdezma/sbt/me/github/Contributors.scala
+++ src/main/scala/com/alejandrohdezma/sbt/me/github/Contributors.scala
@@ -1,4 +1,15 @@
 package com.alejandrohdezma.sbt.me.github
 
 /** Represents a repository's list of contributors */
-final case class Contributors(list: List[Contributor])
+final case class Contributors(list: List[Contributor]) {
+
+  /** Returns this list of contributors in markdown format */
+  lazy val markdown: String =
+    list.map { contributor =>
+      val image =
+        contributor.avatar.map(url => s"![${contributor.login}]($url&s=20) ").getOrElse("")
+
+      s"""- [$image**${contributor.login}**](${contributor.url})"""
+    }.mkString("\n")
+
+}
