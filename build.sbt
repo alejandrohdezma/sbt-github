89ed36028843b1d9c2ffb7e71911ae73c14d5822
Alejandro Hernández <info@alejandrohdezma.com>
2020-01-13 10:14:01 +0100

    Move `withServer` method to package object
    Also remove dependency with `specs2-cats`

--- build.sbt
+++ build.sbt
@@ -5,6 +5,5 @@ enablePlugins(SbtPlugin, MdocPlugin)
 mdocVariables := Map("VERSION" -> version.value.replaceAll("\\+.*", ""))
 
 libraryDependencies += "org.specs2" %% "specs2-core"         % "4.8.3"   % Test
-libraryDependencies += "org.specs2" %% "specs2-cats"         % "4.8.3"   % Test
 libraryDependencies += "org.http4s" %% "http4s-dsl"          % "0.20.15" % Test
 libraryDependencies += "org.http4s" %% "http4s-blaze-server" % "0.20.15" % Test
--- src/test/scala/com/alejandrohdezma/sbt/me/http/ClientSpec.scala
+++ src/test/scala/com/alejandrohdezma/sbt/me/http/ClientSpec.scala
@@ -1,52 +1,34 @@
 package com.alejandrohdezma.sbt.me.http
 
-import cats.effect._
 import cats.implicits._
 
 import com.alejandrohdezma.sbt.me.json.Decoder
 import com.alejandrohdezma.sbt.me.syntax.json.JsonValueOps
-import org.http4s._
+import com.alejandrohdezma.sbt.me.withServer
 import org.http4s.dsl.io._
 import org.http4s.headers.Authorization
-import org.http4s.implicits._
-import org.http4s.server.blaze._
-import org.specs2.matcher.IOMatchers
 import org.specs2.mutable.Specification
 
-class ClientSpec extends Specification with IOMatchers {
+class ClientSpec extends Specification {
 
   "client.get" should {
 
-    "make `GET` call using auth and returning content as `String`" >> {
-      final case class Auth(auth: String)
-
-      implicit val decoder: Decoder[Auth] = _.get[String]("auth").map(Auth)
-
-      val result = withServer(uri => client.get[Auth]((uri / "hello").renderString))
-
-      result must beRight(Auth("Authorization: token 1234"))
-    }
-
-  }
-
-  implicit private val auth: Authentication = Authentication.Token("1234")
-
-  private def withServer[A](f: Uri => A): A = {
-    val routes = HttpRoutes.of[IO] {
+    "make `GET` call using auth and returning content as `String`" >> withServer {
       case req @ GET -> Root / "hello" =>
         Ok(s"""{
           "auth": "${req.headers.get(Authorization).map(_.renderString).orEmpty}"
         }""")
+    } { uri =>
+      final case class Auth(auth: String)
+
+      implicit val decoder: Decoder[Auth] = _.get[String]("auth").map(Auth)
+      implicit val auth: Authentication   = Authentication.Token("1234")
+
+      val result = client.get[Auth](s"${uri}hello")
+
+      result must beRight(Auth("Authorization: token 1234"))
     }
 
-    BlazeServerBuilder[IO]
-      .bindAny()
-      .withHttpApp(routes.orNotFound)
-      .resource
-      .map(_.baseUri)
-      .map(f)
-      .use(IO.pure)
-      .unsafeRunSync()
   }
 
 }
