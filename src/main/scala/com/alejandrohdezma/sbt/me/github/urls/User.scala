ffea6671dc77c7e69a8ae00303d37e6b169fce5f
Alejandro Hernández <info@alejandrohdezma.com>
2020-01-14 10:37:28 +0100

    Get user url from github API entry point

---
+++ src/main/scala/com/alejandrohdezma/sbt/me/github/urls/User.scala
@@ -0,0 +1,22 @@
+package com.alejandrohdezma.sbt.me.github.urls
+
+import com.alejandrohdezma.sbt.me.http.client
+import com.alejandrohdezma.sbt.me.json.Decoder
+import com.alejandrohdezma.sbt.me.syntax.json._
+
+final case class User(base: String) {
+
+  def get(login: String): String = base.replace("{user}", login)
+
+}
+
+object User {
+
+  implicit lazy val user: User =
+    client
+      .get[User]("https://api.github.com")
+      .getOrElse(sys.error("Unable to connect to Github"))
+
+  implicit val UserUrlDecoder: Decoder[User] = json => json.get[String]("user_url").map(User(_))
+
+}
