8275bb50acbf5d7dbc862e9382db5fa05b7a1cf2
Alejandro Hernández <info@alejandrohdezma.com>
2021-09-25 20:19:48 +0200

    Create class representing releases from GitHub API

---
+++ modules/sbt-github/src/main/scala/com/alejandrohdezma/sbt/github/github/Release.scala
@@ -0,0 +1,20 @@
+package com.alejandrohdezma.sbt.github.github
+
+import java.time.ZonedDateTime
+
+import com.alejandrohdezma.sbt.github.json.Decoder
+import com.alejandrohdezma.sbt.github.syntax.json._
+
+/** Represents a repository release */
+final case class Release(name: String, tag: String, publishDate: Option[ZonedDateTime])
+
+object Release {
+
+  implicit val ReleaseDecoder: Decoder[Release] = json =>
+    for {
+      name        <- json.get[String]("name")
+      tag         <- json.get[String]("tag_name")
+      publishDate <- json.get[Option[ZonedDateTime]]("published_at")
+    } yield Release(name, tag, publishDate)
+
+}
